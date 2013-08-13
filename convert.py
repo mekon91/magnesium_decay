40faf1418d9d279331a53ae49d130b00b978b42b
Oscar Byrne <oscar.byrne@googlemail.com>
2013-08-13 11:07:08 +0200

    removed normalisation in raw2numpy function

--- convert.py
+++ convert.py
@@ -13,38 +13,26 @@ def _get_dims(file):
 
 #returns a 2 dimensional numpy array from given .sli file
 def raw2Numpy(filename):
+	
 	if not filename.endswith('.sli'):
 		raise IOError('Bad filetype')
 	
 	with open(filename, 'rb') as file:
 		array = np.empty( _get_dims(file), dtype = float )
-		
-		maxfp = 0							#for normalisation of image
 		fp_index = 0
 		fp = unpack('f', file.read(4))		#NB. unpack returns a tuple, hence fp[0]
-		while fp_index < width * height - 1:
-			#parsing data and setting up for normalisation
-			if fp[0] <= 0:
-				fp = (0,0)
-			else:
-				if fp[0] > maxfp:
-					maxfp = fp[0]
-			
-			array[fp_index // width][fp_index % width] = fp[0]
 		
+		while fp_index < width * height - 1:			
+			array[fp_index // width][fp_index % width] = fp[0]
 			fp = unpack('f', file.read(4))
 			fp_index += 1
-
-		#normalising the array
-		array /= maxfp
-		array *= 255
 		
 		return array
 
 
 if __name__=='__main__':
+	
 	if len(sys.argv) < 2:
 		raise IOError('No filename specified')
+		
 	raw2Numpy(sys.argv[1])
-			
-			
